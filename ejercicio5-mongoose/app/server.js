7a07e8e7cae71f0d7eecffd685fb2a7084e58490
juanda <juandacorreo@gmail.com>
2016-05-18 14:36:33 +0200

    ejercicio 5 sin tests

--- ejercicio5-mongoose/app/server.js
+++ ejercicio5-mongoose/app/server.js
@@ -1,35 +1,19 @@
 var express = require('express') //llamamos a Express
-var app = express()                
+var bodyParser = require('body-parser')
+/*toda la configuración de bbdd la hacemos en un fichero a parte*/
+require('./db')
 
+var app = express()                
 var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
-// para establecer las distintas rutas, necesitamos instanciar el express router
-var router = express.Router() 
-
-var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())            
 
-//establecemos nuestra primera ruta, mediante get.
-router.get('/', function(req, res) {
-    res.json({ mensaje: '¡Hola Mundo!' })  
-})
-
-router.get('/:nombre', function(req, res) {
-    res.json({ mensaje: '¡Hola ' + req.params.nombre })  
-})
-// ojo, en el post la variable se obtiene del body que envía bodyParser, no de los
-// parámetros de la url
-router.post('/',function(req,res) { 
-    res.json({mensaje: req.body.nombre})  
-})
-
-
+// ahora en vez de llamar a las rutas desde aquí, lo haremos en un fichero separado
 // nuestra ruta irá en http://localhost:8080/api
 // es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+const router = require('./routes');
 app.use('/api', router)
 
-// START THE SERVER
-// =============================================================================
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
